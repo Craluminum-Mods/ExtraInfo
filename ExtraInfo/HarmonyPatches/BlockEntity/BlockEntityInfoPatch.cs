3c3c7ab49ab07199867e5d1aa00c967166c397f0
Dana <69315569+Craluminum2413@users.noreply.github.com>
2026-05-03 20:00:17 +0300

    added progress bar for transient block info

--- ExtraInfo/HarmonyPatches/BlockEntity/BlockEntityInfoPatch.cs
+++ ExtraInfo/HarmonyPatches/BlockEntity/BlockEntityInfoPatch.cs
@@ -8,7 +8,6 @@ public static class BlockEntityInfoPatch
     public static void Postfix(BlockEntity __instance, StringBuilder dsc)
     {
         dsc.GetBombInfo(null, __instance as BlockEntityBomb);
-        dsc.GetTransientInfo(__instance as BlockEntityTransient);
         dsc.GetMechanicalBlockInfo(__instance);
     }
 }
---
+++ ExtraInfo/Systems/BlockTransitionInfo/BlockTransitionInfoPatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExtraInfo.Systems.BlockTransitionInfo;
+
+[HarmonyPatch(typeof(BlockEntity), nameof(BlockEntity.GetBlockInfo))]
+public static class BlockTransitionInfoPatch
+{
+    [HarmonyPostfix]
+    public static void Postfix(BlockEntity __instance, StringBuilder dsc)
+    {
+        if (Core.Config?.ShowBlockTransitionInfo != true) return;
+
+        if (__instance is not BlockEntityTransient blockEntity) return;
+
+        TransientProperties props = blockEntity.GetField<TransientProperties>("props");
+        if (props == null) return;
+
+        blockEntity.CheckTransition(0);
+
+        double hoursLeft = blockEntity.GetField<double>("transitionHoursLeft");
+
+        if (hoursLeft > 0)
+        {
+            float totalHours = props.InGameHours;
+
+            float completedPercent = (float)Math.Clamp((1.0 - (hoursLeft / totalHours)) * 100, 0, 100);
+
+            double igSeconds = hoursLeft * 3600;
+            float speedOfTime = blockEntity.Api.World.Calendar.SpeedOfTime;
+
+            string verticalBlock = TimeFormatter.BuildVerticalTimeBlock(
+                igSeconds,
+                speedOfTime,
+                completedPercent,
+                Lang.Get("extrainfo:TimeUntilTransition"));
+
+            dsc.AppendLine().Append(verticalBlock);
+        }
+    }
+}
