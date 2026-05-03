84f5e8d0d6d8b71429c46a6ef32e57a25108a46f
Dana <69315569+Craluminum2413@users.noreply.github.com>
2026-05-03 12:49:17 +0300

    added null check for sapling description

--- ExtraInfo/src/CollectibleBehavior/CollectibleBehaviorTreeGrowthDescription.cs
+++ ExtraInfo/src/CollectibleBehavior/CollectibleBehaviorTreeGrowthDescription.cs
@@ -16,6 +16,8 @@ public class CollectibleBehaviorTreeGrowthDescription : CollectibleBehavior
 
     private static void AppendInfo(ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world)
     {
+        if (inSlot?.Itemstack?.Collectible == null) return;
+
         NatFloat sproutDays = null;
         NatFloat matureDays = null;
 
@@ -23,12 +25,15 @@ public class CollectibleBehaviorTreeGrowthDescription : CollectibleBehavior
         {
             Block block = world.GetBlock($"{item.Code.Domain}:sapling-{item.Variant["type"]}-free");
             if (block == null) return;
+            if (block.Attributes == null) return;
 
             sproutDays = block.Attributes[Text.SproutDaysAttr].AsObject<NatFloat>();
             matureDays = block.Attributes[Text.MatureDaysAttr].AsObject<NatFloat>();
         }
         else if (inSlot.Itemstack.Collectible is BlockPlant block && !string.IsNullOrEmpty(block.EntityClass))
         {
+            if (block.Attributes == null) return;
+
             sproutDays = block.Attributes[Text.SproutDaysAttr].AsObject<NatFloat>();
             matureDays = block.Attributes[Text.MatureDaysAttr].AsObject<NatFloat>();
         }
