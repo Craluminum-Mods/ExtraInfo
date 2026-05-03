f53b0e31f0f10f8754c1ca704281c60245730793
Dana <69315569+Craluminum2413@users.noreply.github.com>
2026-05-03 21:28:20 +0300

    added progress bar to beehive kiln info and simplified its info a lot

--- ExtraInfo/HarmonyPatches/PlacedBlockInfoPatch.cs
+++ ExtraInfo/HarmonyPatches/PlacedBlockInfoPatch.cs
@@ -9,6 +9,5 @@ public static class PlacedBlockInfoPatch
     {
         __result = __result.GetCokeInfo(world, pos);
         __result = __result.GetSteelInfo(world, pos);
-        __result = __result.GetBeehiveKilnInfo(world, pos);
     }
 }
